6756496c4e63c4982ef7ccdf341568eb14e4bb24
skmdashan@gmail.com <ashanshaik987@gmail.com>
2025-09-15 15:09:17 +0530

    fix all the bugs

---
+++ src/declarations.d.ts
@@ -0,0 +1,5 @@
+declare module "*.jsx";
+declare module "*.js";
+declare module './redux/store' {
+  export const store: any;
+}
---
+++ src/redux/store.d.ts
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './slices/cartSlice';
+import productReducer from './slices/productSlice';
+
+export const store: ReturnType<typeof configureStore>;
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
